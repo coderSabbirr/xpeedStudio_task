2acb2231a5e047393905229ea690eee3b37d8e97
coderSabbirr <sabirnahid5817@gmail.com>
2022-03-13 13:42:08 +0600

    table drag and drop add

--- src/Component/Page/tableList/TableLists.js
+++ src/Component/Page/tableList/TableLists.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 const TableList = () => {
   const [tableDatas, setTableDatas] = useState([]);
   const [displayData, setDisplayData] = useState([]);
   const [order, setOrder] = useState("ASC");
+
   useEffect(() => {
     fetch(`http://localhost/api/list.php`)
       .then((res) => res.json())
@@ -13,6 +15,21 @@ const TableList = () => {
       });
   }, []);
 
+  const handleDragEnd = (e) => {
+    console.log(e);
+
+    if (!e.destination) return;
+    let tempData = Array.from(displayData);
+    let [source_data] = tempData.splice(e.source.index, 1);
+    tempData.splice(e.destination.index, 0, source_data);
+    setDisplayData(tempData);
+
+    alert("test");
+  };
+
+  // search functionality start
+
+  // search id
   const handleSearchById = (e) => {
     const searchId = e.target.value;
 
@@ -22,6 +39,8 @@ const TableList = () => {
     setDisplayData(machedId);
   };
 
+  //search by name
+
   const handleSearchByName = (e) => {
     const searchName = e.target.value;
     const machedName = tableDatas.data?.rows?.filter((item) =>
@@ -29,6 +48,18 @@ const TableList = () => {
     );
     setDisplayData(machedName);
   };
+  const handleSearchByMessage = (e) => {
+    const searchName = e.target.value;
+    const machedName = tableDatas.data?.rows?.filter((item) =>
+      item?.message?.toLowerCase().includes(searchName.toLowerCase())
+    );
+    setDisplayData(machedName);
+  };
+
+  // search functionality end
+
+  // sorted by name,message,date start
+
   const sorting = (col) => {
     if (order === "ASC") {
       const sorted = [...displayData].sort((a, b) =>
@@ -47,6 +78,11 @@ const TableList = () => {
       setOrder("ASC");
     }
   };
+  // sorted by name,message,date end
+
+  const dargChange = (e) => {
+    console.log("hit me");
+  };
   return (
     <div
       className="w-50 mt-5"
@@ -63,47 +99,83 @@ const TableList = () => {
         />
         <input
           type="text"
-          className="form-control"
+          className="form-control  me-2"
           placeholder="Search Name"
           aria-label=""
           aria-describedby="basic-addon1"
           onChange={handleSearchByName}
         />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search Message"
+          aria-label=""
+          aria-describedby="basic-addon1"
+          onChange={handleSearchByMessage}
+        />
       </div>
-      <table className="table table-bordered">
-        <thead>
-          {tableDatas.data?.headers?.map((pd, index) => (
-            <tr key={pd.id}>
-              {pd.id.hidden === false && <th scope="col">{pd?.id.title}</th>}
-              {pd.name.hidden === false && (
-                <th scope="col" onClick={() => sorting("name")}>
-                  {pd?.name.title}
-                </th>
-              )}
-              {pd.message.hidden === false && (
-                <th scope="col" onClick={() => sorting("message")}>
-                  {pd?.message.title}
-                </th>
-              )}
-              {pd.created_at.hidden === false && (
-                <th scope="col" onClick={() => sorting("created_at")}>
-                  {pd?.created_at.title}
-                </th>
-              )}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {displayData.map((pd, index) => (
-            <tr key={pd.id}>
-              <td>{pd.id}</td>
-              <td>{pd.name}</td>
-              <td>{pd.message}</td>
-              <td>{pd.created_at}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <table className="table table-bordered">
+          <thead>
+            {tableDatas.data?.headers?.map((pd, index) => (
+              <tr key={pd.id}>
+                {pd.id.hidden === false && <th scope="col">{pd?.id.title}</th>}
+                {pd.name.hidden === false && (
+                  <th scope="col" onClick={() => sorting("name")}>
+                    {pd?.name.title}
+                  </th>
+                )}
+                {pd.message.hidden === false && (
+                  <th scope="col" onClick={() => sorting("message")}>
+                    {pd?.message.title}
+                  </th>
+                )}
+                {pd.created_at.hidden === false && (
+                  <th scope="col" onClick={() => sorting("created_at")}>
+                    {pd?.created_at.title}
+                  </th>
+                )}
+              </tr>
+            ))}
+          </thead>
+          <Droppable droppableId="droppable-1">
+            {(provider) => (
+              <tbody
+                className="text-capitalize"
+                ref={provider.innerRef}
+                {...provider.droppableProps}
+                onChange={dargChange}
+              >
+                {displayData?.map((user, index) => (
+                  <Draggable
+                    key={user.name}
+                    draggableId={user.name}
+                    index={index}
+                  >
+                    {(provider) => (
+                      <tr {...provider.draggableProps} ref={provider.innerRef}>
+                        <td {...provider.dragHandleProps} onClick={dargChange}>
+                          {user.id}
+                        </td>
+                        <td {...provider.dragHandleProps} onClick={dargChange}>
+                          {user.name}
+                        </td>
+                        <td {...provider.dragHandleProps} onClick={dargChange}>
+                          {user.message}
+                        </td>
+                        <td {...provider.dragHandleProps} onClick={dargChange}>
+                          {user.created_at}
+                        </td>
+                      </tr>
+                    )}
+                  </Draggable>
+                ))}
+                {provider.placeholder}
+              </tbody>
+            )}
+          </Droppable>
+        </table>
+      </DragDropContext>
     </div>
   );
 };
